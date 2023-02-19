b372acf0bd771d585a59dad837086109144dc9a9
nitesh <nkjangid03@gmail.com>
2023-02-19 22:15:42 +0530

    migration_table_orders

---
+++ database/migrations/2023_02_19_140248_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('course_id');
+            $table->decimal('course_price', 10, 2);
+            $table->dateTime('purchase_date');
+            $table->decimal('discount', 10, 2)->nullable();
+            $table->string('txn_id');
+            $table->string('charge_id')->nullable();
+            $table->string('payment_status')->default(0);
+            $table->string('payment_method');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
